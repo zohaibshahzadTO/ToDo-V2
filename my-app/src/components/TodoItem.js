a3e81e210cc456dca46a6f36acc7bf8634391498
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-03-11 19:44:10 -0400

    Mass Update + Completion
    - Components Creation
    - Prop Drilling
    - TodoItem, TodoInput, TodoList

---
+++ my-app/src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class TodoItem extends Component {
+  render() {
+    const {title, handleDelete, handleEdit} = this.props
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6>{title}</h6>
+        <div className="todo-icon">
+          <span className="mx-2 text-success">
+            <i className="fas fa-pen" onClick={handleEdit}/>
+          </span>
+          <span className="mx-2 text-danger" onclick={handleDelete}>
+            <i className="fas fa-trash" />
+          </span>
+        </div>
+      </li>
+    )
+  }
+}
